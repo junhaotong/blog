e02582fa1d34428dfbe885937b58c4455a2ea22f
hao <hao@bluereader.cn>
2017-08-16 23:17:35 +0800

    push

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,13 @@
 var express = require('express');
 var router = express.Router();
-// let jwt = require('jsonwebtoken');
 
 /* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index.html');
+});
+/* GET admin page. */
 router.get('/admin', function (req, res, next) {
     res.render('admin.html');
-    // pool.getConnection((err, connection) => {
-    //     let token = jwt.sign({name: '俊豪'}, 'jeremy');
-    //     res.cookie('_token', ''+token, { expires: new Date(Date.now() + 900000) });
-    //     connection.query('show tables', (err, result) => {
-    //         res.send(token);
-    //         res.render('admin.html');
-    //         connection.release();
-    //     });
-    // });
 });
 
 module.exports = router;
