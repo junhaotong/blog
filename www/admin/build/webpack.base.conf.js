c907dec8669a7909dc54c6bc4a7dcd2d1d7565ee
童俊豪 <hao@bluereader.cn>
2017-08-19 20:59:19 +0800

    'push'

--- www/admin/build/webpack.base.conf.js
+++ www/admin/build/webpack.base.conf.js
@@ -58,7 +58,7 @@ module.exports = {
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+          name: utils.assetsPath('/stylesheets/fonts/[name].[hash:7].[ext]')
         }
       }
     ]
