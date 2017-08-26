73d5c7926484c8835aed3889f4e03ad3614e8958
hao <hao@bluereader.cn>
2017-08-27 01:46:37 +0800

    add

--- web/admin/config/index.js
+++ web/admin/config/index.js
@@ -7,7 +7,7 @@ module.exports = {
         index: path.resolve(__dirname, '../../../view/admin.html'),
         assetsRoot: path.resolve(__dirname, '../../../www/static'),
         assetsSubDirectory: '',
-        assetsPublicPath: 'static/',
+        assetsPublicPath: '/static/',
         productionSourceMap: false,
         // Gzip off by default as many popular static hosts such as
         // Surge or Netlify already gzip all static assets for you.
