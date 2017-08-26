df5998115155b0b76e92ec508f2b15d2adda5b6e
hao <hao@bluereader.cn>
2017-08-27 01:57:46 +0800

    add

--- src/config/middleware.js
+++ src/config/middleware.js
@@ -1,37 +1,41 @@
 const path = require('path');
 const isDev = true;
+const compression = require('compression')
 
 module.exports = [
-  {
-    handle: 'meta',
-    options: {
-      logRequest: isDev,
-      sendResponseTime: isDev
-    }
-  },
-  {
-    handle: 'resource',
-    enable: isDev,
-    options: {
-      root: path.join(think.ROOT_PATH, 'www'),
-      publicPath: /^\/(static|favicon\.ico)/
-    }
-  },
-  {
-    handle: 'trace',
-    enable: !think.isCli,
-    options: {
-      debug: isDev
-    }
-  },
-  {
-    handle: 'payload',
-    options: {}
-  },
-  {
-    handle: 'router',
-    options: {}
-  },
-  'logic',
-  'controller'
+    {
+        handle: compression()
+    },
+    {
+        handle: 'meta',
+        options: {
+            logRequest: isDev,
+            sendResponseTime: isDev
+        }
+    },
+    {
+        handle: 'resource',
+        enable: isDev,
+        options: {
+            root: path.join(think.ROOT_PATH, 'www'),
+            publicPath: /^\/(static|favicon\.ico)/
+        }
+    },
+    {
+        handle: 'trace',
+        enable: !think.isCli,
+        options: {
+            debug: isDev
+        }
+    },
+    {
+        handle: 'payload',
+        options: {}
+    },
+    {
+        handle: 'router',
+        options: {}
+    },
+    'logic',
+    'controller'
 ];
