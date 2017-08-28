64319909c24346ef56d5d6ee02132ba7b63044cc
hao <hao@bluereader.cn>
2017-08-28 09:50:37 +0800

    add

--- app/config/router.js
+++ app/config/router.js
@@ -1,1 +1,1 @@
-module.exports = [[/^\//, 'index', 'get'], [/^\/admin/, 'index/admin', 'get'], ['/api/admin/login', 'admin/login', 'post'], ['/api/regist', 'front/regist', 'post'], ['/api/upload/image', 'upload/image', 'post'], ['/webhook', 'webhook', 'post']];
+module.exports = [[/^\/admin/, 'index/admin', 'get'], ['/api/admin/login', 'admin/login', 'post'], ['/api/regist', 'front/regist', 'post'], ['/api/upload/image', 'upload/image', 'post'], ['/webhook', 'webhook', 'post']];
