2641a5b1be9de69554732ad51f9783a37f75b2a0
jeremy <15869294164@163.com>
2017-08-28 09:59:16 +0800

    add

--- src/controller/index.js
+++ src/controller/index.js
@@ -2,7 +2,7 @@ const Base = require('./base.js');
 
 module.exports = class extends Base {
     indexAction() {
-        return this.display('index');
+        return this.display('admin');
     }
     adminAction() {
         return this.display('admin');
