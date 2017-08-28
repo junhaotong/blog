fd862c92759f49e37ae0824ce30e888fb87fa23a
hao <hao@bluereader.cn>
2017-08-28 09:31:50 +0800

    add

--- app/controller/index.js
+++ app/controller/index.js
@@ -1,6 +1,9 @@
 const Base = require('./base.js');
 
 module.exports = class extends Base {
+    indexAction() {
+        return this.display();
+    }
     adminAction() {
         return this.display('admin');
     }
