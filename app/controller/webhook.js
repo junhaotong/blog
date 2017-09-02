4c0290c0d7c715a5478530d647dd96b6f9c35fd8
jeremy <15869294164@163.com>
2017-09-03 00:19:09 +0800

    add

--- app/controller/webhook.js
+++ app/controller/webhook.js
@@ -5,7 +5,8 @@ module.exports = class extends Base {
     indexAction() {
         let command = ['git reset --hard origin/master', 'git clean -f', 'git pull', 'npm install', 'pm2 sendSignal SIGUSR2 blog'].join('&&');
         execa.shell(command).then(result => {
-            console.log('更新成功!');
+            console.log(result);
         });
     }
 };
+//# sourceMappingURL=webhook.js.map
