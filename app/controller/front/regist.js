07ea569a4bc8cc7bf2eb752bff834b62ec450aa0
jeremy <15869294164@163.com>
2017-09-03 00:24:10 +0800

    add

--- app/controller/front/regist.js
+++ app/controller/front/regist.js
@@ -5,7 +5,7 @@ module.exports = class extends Base {
         let userService = this.service('user');
         let user_id = userService.addUser(this.post('username'), this.post('email'), think.md5(this.post('password')), this.post('avatar'), 1, 0);
         if (user_id) {
-            return this.success('注册成功');
+            return this.success(null, '注册成功');
         } else {
             return this.fail(1000, '注册失败');
         }
