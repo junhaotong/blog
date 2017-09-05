4861c127eba8411fed9d419f00b270e8b04a0e1e
hao <hao@bluereader.cn>
2017-09-05 23:43:03 +0800

    add

--- src/controller/front/login.js
+++ src/controller/front/login.js
@@ -6,7 +6,7 @@ module.exports = class extends Base {
         let success = await captchasController.validateCaptchas();
         if (success) {
             // 验证码二次验证通过
-            return this.success('验证通过');
+            return this.success(null, '验证通过');
         } else {
             return this.fail(1000, '验证码错误或已失效');
         }
