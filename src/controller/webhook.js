a3b6b2bf6d09d8881cbb7b590494e0a32521cdce
jeremy <15869294164@163.com>
2017-10-04 19:52:31 +0800

    add

--- src/controller/webhook.js
+++ src/controller/webhook.js
@@ -3,7 +3,7 @@ const execa = require('execa');
 
 module.exports = class extends Base {
     indexAction() {
-        let command = ['git reset --hard origin/master', 'git clean -f', 'git pull', 'npm install', 'pm2 reload all'].join('&&')
+        let command = ['git reset --hard origin/master', 'git clean -f', 'git pull', 'npm install', 'npm run compile', 'pm2 reload all'].join('&&')
         execa.shell(command).then(result => {
             console.log(result);
         });
