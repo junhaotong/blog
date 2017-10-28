055a3f1dfaf29ff79372defd81155ba23d047c35
hao <hao@bluereader.cn>
2017-10-28 12:47:05 +0800

    add

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,21 +1,7 @@
 const Base = require('./base.js');
-const { createBundleRenderer } = require('vue-server-renderer');
-const serverBundle = require('./../../www/vue-ssr-server-bundle.json');
 
 module.exports = class extends Base {
     async indexAction() {
-        // const renderer = createBundleRenderer(serverBundle, {
-        //     runInNewContext: false, // 推荐
-        //     clientManifest
-        // })
-        // const context = { url: this.ctx.req.url };
-        // await new Promise((resolve, reject) => {
-        //     renderer.renderToString(context, (err, html) => {
-        //         console.log(html);
-        //         this.assign('html', html);
-        //         resolve();
-        //     })
-        // })
         return this.display('index');
     }
     adminAction() {
