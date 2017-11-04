8232a82051c28543cd1ee039cbe64a4238dcf762
jeremy <15869294164@163.com>
2017-11-04 22:12:13 +0800

    add

--- web/front/src/components/Modules/Content/index.js
+++ web/front/src/components/Modules/Content/index.js
@@ -1,6 +1,6 @@
 /**
  * Created by apple on 2017/9/7.
  */
-// import Content from './Content.vue';
-const Content = resolve => require(['./Content.vue'], resolve);
+import Content from './Content.vue';
+// const Content = resolve => require(['./Content.vue'], resolve);
 export default Content;
